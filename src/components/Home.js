335aaa11af26714aab3ed6fc8ee55f644aaf7547
‘Juliano <julianoperins@gmail.com>
2020-10-31 14:15:14 -0400

    Created a custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-// API
-import API from "../API";
-
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
@@ -14,33 +11,6 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-
-      // Set the State
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }));
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  };
-
-  // The brackets are called dependency array for the useEffect
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-
   console.log(state);
   return <div>Home page</div>;
 };
