5aa938a87ee86391128f95416f5ba327aae1ef2c
‘Juliano <julianoperins@gmail.com>
2020-10-31 12:11:05 -0400

    fetched data from the API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 // Styles
 import { GlobalStyle } from "./GlobalStyle";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <GlobalStyle />
     </div>
   );
