8831157969ddd32a1275e5c24a209abf960b32fa
‘Juliano <julianoperins@gmail.com>
2020-11-11 10:14:39 -0500

    Displayed the banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
+import HeroImage from "./HeroImage";
 
 // Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -14,7 +15,17 @@ import NoImage from "../images/no_image.jpg";
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
-  return <div>Home page</div>;
+  return (
+    <>
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[1].backdrop_path}`}
+          title={state.results[1].title}
+          text={state.results[1].overview}
+        />
+      )}
+    </>
+  );
 };
 
 export default Home;
