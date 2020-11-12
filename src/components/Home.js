de821c57c6da86cea6fef9dbe666863916fe88bf
‘Juliano <julianoperins@gmail.com>
2020-11-12 11:13:35 -0500

    implemented pagination

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,16 +18,27 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+  const {
+    state,
+    loading,
+    error,
+    searchTerm,
+    setSearchTerm,
+    setIsLoadingMore,
+  } = useHomeFetch();
+
   // console.log(state.results);
+
+  if (error) return <div>Something went wrong ...</div>;
+
   return (
     <>
       <SearchBar setSearchTerm={setSearchTerm} />
-      {!searchTerm && state.results[1] ? (
+      {!searchTerm && state.results[0] ? (
         <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[1].backdrop_path}`}
-          title={state.results[1].title}
-          text={state.results[1].overview}
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].title}
+          text={state.results[0].overview}
         />
       ) : null}
       <Grid
@@ -48,7 +59,10 @@ const Home = () => {
       </Grid>
       {loading && <Spinner />}
       {state.page < state.total_pages && !loading && (
-        <Button text="Fetch More Movies" />
+        <Button
+          text="Fetch More Movies"
+          callback={() => setIsLoadingMore(true)}
+        />
       )}
     </>
   );
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -16,6 +16,7 @@ export const useHomeFetch = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
 
   const fetchMovies = async (page, searchTerm = "") => {
     try {
@@ -23,7 +24,7 @@ export const useHomeFetch = () => {
       setLoading(true);
       const movies = await API.fetchMovies(searchTerm, page);
 
-      // Set the State
+      // Set the State according the pages
       setState((prev) => ({
         ...movies,
         results:
@@ -41,6 +42,15 @@ export const useHomeFetch = () => {
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
+  // console.log(state.page);
 
-  return { state, loading, error, searchTerm, setSearchTerm };
+  // Load More
+  useEffect(() => {
+    if (!isLoadingMore) return;
+    //! Verify the pages later...
+    fetchMovies(2, searchTerm);
+    setIsLoadingMore(false);
+  }, [isLoadingMore, searchTerm, state]);
+
+  return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
