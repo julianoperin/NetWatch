c1c833d11690163f2c5bd1d5f56d4dfe80e4c55a
‘Juliano <julianoperins@gmail.com>
2020-11-13 11:17:47 -0500

    Fixed the home Bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
-
 // Components
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
-import { Spinner } from "./Spinner/Spinner.styles";
-import SearchBar from "./SearchBar/Index";
+import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 import Button from "./Button";
 
-// Hooks
+// Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
-
 // Image
 import NoImage from "../images/no_image.jpg";
 
@@ -27,23 +24,21 @@ const Home = () => {
     setIsLoadingMore,
   } = useHomeFetch();
 
-  // console.log(state.results);
+  // console.log(state);
 
   if (error) return <div>Something went wrong ...</div>;
 
   return (
     <>
-      <SearchBar setSearchTerm={setSearchTerm} />
-      {!searchTerm && state.results[1] ? (
+      {!searchTerm && state.results[0] ? (
         <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[1].backdrop_path}`}
-          title={state.results[1].title}
-          text={state.results[1].overview}
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
         />
       ) : null}
-      <Grid
-        header={searchTerm ? `Results for " ${searchTerm}"` : "Featured Movies"}
-      >
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
         {state.results.map((movie) => (
           <Thumb
             key={movie.id}
@@ -59,10 +54,7 @@ const Home = () => {
       </Grid>
       {loading && <Spinner />}
       {state.page < state.total_pages && !loading && (
-        <Button
-          text="Fetch More Movies"
-          callback={() => setIsLoadingMore(true)}
-        />
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
   );
